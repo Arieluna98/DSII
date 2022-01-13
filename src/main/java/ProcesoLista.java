2bc5ed24f7b8fe32caa07c023b2bd747d2e7e3d0
Ariel <arieluna99@gmail.com>
2022-01-13 12:51:16 -0700

    Proceso lista - Hola gg

--- src/main/java/ProcesoLista.java
+++ src/main/java/ProcesoLista.java
@@ -1,23 +1,31 @@
 public class ProcesoLista {
     public static void main(String[] args) {
+        System.out.println("Holagg");
         if(args.length == 0){
             System.out.println("Faltan argumentos");
             System.exit(1);
         }
 
         int suma = 0, media = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        for (String arg : args) {
+        int i = 0;
+        while (i < args.length) {
+            String arg = args[i];
             int x = 0;
             try {
+
+
+
                 x = Integer.parseInt(arg);
             } catch (NumberFormatException ex) {
                 System.out.println("Error en formato de numeros.");
                 System.exit(2);
             }
 
+
             min = Math.min(min, x);
             max = Math.max(max, x);
             suma = suma + x;
+            i++;
         }
 
         System.out.println("Suma: " + suma);
